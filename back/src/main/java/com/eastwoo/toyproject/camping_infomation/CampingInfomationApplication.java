7e1301ee5953c5f7b8c1b4c25c449b5bf890a25d
dongwoo <dwseo96@wavus.co.kr>
2025-01-02 16:52:42 +0900

    :tada: 프로젝트 시작!

---
+++ back/src/main/java/com/eastwoo/toyproject/camping_infomation/CampingInfomationApplication.java
@@ -0,0 +1,11 @@
+package com.eastwoo.toyproject.camping_infomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingInfomationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampingInfomationApplication.class, args);
+    }
+}
---
+++ back/src/test/java/com/eastwoo/toyproject/camping_infomation/CampingInfomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastwoo.toyproject.camping_infomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingInfomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
